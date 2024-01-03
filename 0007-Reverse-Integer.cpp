e7d5b536786b7fbfab5e97a2ceb4a60483bd1466
Ojas Patil <128805590+Patil-Ojas@users.noreply.github.com>
2024-01-03 16:19:32 +0530

    Add files via upload

---
+++ 0007-Reverse-Integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0)
+        {
+            if ((rev > INT_MAX/10) or (rev < INT_MIN/10))
+            {
+                return 0;
+            }
+            else
+            {
+                int rem = x%10;
+                rev = rev*10 + rem;
+                x = x/10;
+            }
+        }
+
+        return rev;
+    }
+};
